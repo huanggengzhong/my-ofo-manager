67ea928231fc6dd8483ed636711fc4ed7c13938a
huanggengzhong <you@example.com>
2019-08-05 10:29:04 +0800

    增加了footer

--- src/admin.js
+++ src/admin.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { Row, Col } from 'antd'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import  "./style/common.less"
 
 class Admin extends Component {
@@ -11,11 +12,14 @@ class Admin extends Component {
         <Col span={3} className="nav-left">left</Col>
         <Col span={21} className="main">
          <Header >
-             头部
+             
          </Header>
          <Row className="content">
              内容
          </Row>
+         <Footer>
+           
+         </Footer>
         </Col>
       </Row>
     )
---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div >
+
+                <div className="footer">This is Footer</div>
+                
+            </div>
+
+         );
+    }
+}
+ 
+export default Footer;
